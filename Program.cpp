6d2a98cb06e1b683f5223765d661c5c4acbbb156
Blanca <blancanietopetinal@gmail.com>
2020-09-14 09:23:00 +0200

    Funcion add_pixel anadida a la clase Grid

--- Program.cpp
+++ Program.cpp
@@ -4,7 +4,6 @@
 #define M 300
 #define N 300
 // https://processing.org/tutorials/2darray/
-//PROBLEMA: me sobreexcribe el pixel (comparar con cuadrado??)
 class Pixel{
     private:
         char cel_type;
@@ -18,13 +17,13 @@ class Pixel{
 class Grid{
     private:
         int rows, cols;
-        Pixel  *p[M][N], p0 = 'T';
+        Pixel  *p[M][N], p0 = 'R';
     public:
         Grid(); //create the grid
-        Grid(int, int, Pixel); //place the pixel
-       //auto get_grid(){return g;}; //SEGUIR POR AQUI
-        /*void add_pixel(int, int, Pixel);
-        void print_pixel(int, int);*/
+        Grid(int, int, Pixel); //place the pixel???
+        void print_pixel(int, int);
+        ~Grid(){};
+        // Crear addpixel
 
 };
 
@@ -33,26 +32,18 @@ Grid::Grid(){
     cols = N;
     for (int i = 0; i < cols; i++) {
         for (int j = 0; j < rows; j++) {
-        //g[i][j] = p0;
         p[i][j] = &p0;
         }
     }
 };
 
-/*void Grid::add_pixel(int _rows, int _cols, Pixel _p){
-    //p[_rows][_cols] = &_p;
-    //[_rows][_cols] = _p;
-};
-
 void Grid::print_pixel(int x, int y){
     std::cout<<p[x][y]->getType();
 };
-*/
 
 int main(){
-    Pixel p1('T'), p2('H');
-    std::cout<<"Before: "<<p1.getType()<<p2.getType()<<std::endl;
-    Grid g();
-    std::cout<<"After: "<<p1.getType()<<p2.getType()<<std::endl;
-    
+    //Pixel p1('T'), p2('H');
+    Grid g;
+    g.print_pixel(1,1);
+    return 0;
 };
