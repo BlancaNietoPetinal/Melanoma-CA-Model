f8ca2c0f100261994960aa7c2fb899eaf6f339b4
Blanca <blancanietopetinal@gmail.com>
2020-10-22 10:14:21 +0200

    tumor_lysis added

--- DestructionLib/destructionlib.cpp
+++ DestructionLib/destructionlib.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream> //getMat
 #include "destructionlib.h"
+#include <random>
+#include <chrono>
+#include <array>
 
 int * getMat(std::string filename, int matlen){
     int * mat;
@@ -25,6 +28,24 @@ void get_limits(int * mat, int xsize, int ysize, int &left, int &right, int &sup
     sup = superiorBorder(mat, xsize, ysize);
     inf = inferiorBorder(mat, xsize, ysize);
 }
+void node_to_coordinates(int node, int &x, int &y, int xsize, int ysize){
+    int n = 0;
+    bool esc = 0;
+    for(int xcoor = 0; xcoor<xsize; xcoor++){
+        for(int ycoor = 0; ycoor<ysize; ycoor++){
+            if(n == node){
+                x = xcoor;
+                y = ycoor;
+                esc = true;
+                break;
+            }
+            n++;
+        }
+        if(esc){
+            break;
+        }
+    }
+};
 int leftBorder(int * mat, int size){
     int node = 0;
     while(mat[node]==0){
@@ -84,36 +105,23 @@ int * effectorCellPlacement(int leftnode, int rightnode, int supnode, int infnod
     int xleft, xright, ysup, yinf, _;
     mat = new int[xsize*ysize];
 
-    get_coordinates(leftnode, xsize, ysize, xleft, _);
-    get_coordinates(rightnode, xsize, ysize, xright, _);
-    get_coordinates(supnode, xsize, ysize, _, ysup);
-    get_coordinates(infnode, xsize, ysize, _, yinf);
-     //std::cout<<"ysup: "<<ysup<<" yinf: "<<yinf<<" xl: "<<xleft<<" xright: "<<xright<<std::endl;
+    node_to_coordinates(leftnode, xleft, _, xsize, ysize);
+    node_to_coordinates(rightnode, xright, _, xsize, ysize);
+    node_to_coordinates(supnode, _, ysup, xsize, ysize);
+    node_to_coordinates(infnode, _, yinf, xsize, ysize);
     mat = get_squeare(xleft, xright, ysup, yinf, xsize, ysize);
 
     match_matrices(T, mat, xsize, ysize);
     return mat;
 };
-void get_coordinates(int node, int xsize, int ysize, int &xcoor, int &ycoor){
-    int n = 0;
-    for(int x = 0; x<xsize; x++){
-        for(int y = 0; y<ysize; y++){
-            n++;
-            if(n==node){
-                xcoor = x;
-                ycoor = y;
-                break;
-            }
-        }
-    }
-}
+
 int * get_squeare(int xleft, int xright, int ysup, int yinf, int xsize, int ysize){
     int node = 0;
     int * mat;
     mat = new int[xsize*ysize];
     for(int x = 0; x<xsize; x++){
         for(int y = 0; y<ysize; y++){
-            if( ((x>=(xleft-1)) && (y>=ysup)) && ((x<=(xright+1)) && (y<=(yinf+2))) ){
+            if( ((x>=(xleft-1)) && (y>=(ysup-1))) && ((x<=(xright+1)) && (y<=(yinf+2))) ){
                 mat[node] = 1;
             }
             else{
@@ -133,4 +141,69 @@ void match_matrices(int *T,int *mat, int xsize, int ysize){
     }
 };
 
+void tumor_lysis(int * T, int * E, int *D, int xsize, int ysize){
+    std::default_random_engine generator{static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count())};
+    std::uniform_int_distribution<int> dice_distribution(1,3);
+    int dice, node = 0;
+    for(int x = 0; x < xsize; x++)
+    {
+        for(int y = 0; y < ysize; y++)
+        {
+            if(E[node]==1){
+                dice = dice_distribution(generator);
+                switch (dice)
+                {
+                case 1:
+                    cell_lysis(T, E, node, xsize, ysize);
+                    break;
+                
+                case 2:
+                    std::cout<<"OK"<<std::endl;
+                    //recruitment();
+                    break;
 
+                case 3:
+                    std::cout<<"OK"<<std::endl;
+                    //inactivation();
+                }
+            }
+            node++;
+        }
+    }
+};
+
+void cell_lysis(int * T, int *E, int node, int xsize, int ysize){
+    std::vector<int> neighbours;
+    int N = 1;
+    //neighbours = n_neighbours(N, E, node, xsize, ysize);
+};
+
+std::vector<int> n_neighbours(int n, int * mat, int node, int xsize, int ysize){ //intentar cambiarla por la que esta en Generation
+    // Fetches the n closest neighbours
+    std::vector<int> n_neighbours;
+    int x, y;
+
+    if(are_neighbours_interior(node, n, xsize, ysize)){  
+        node_to_coordinates(node, x, y, xsize, ysize);
+        for(int i = (x-n); i<(x+n); i++){
+            for(int j = (y-n); j<(y+n); j++){
+                
+            }
+        }
+
+        std::cout<<"OK"<<std::endl;
+    }
+    return n_neighbours;
+}; 
+
+bool are_neighbours_interior(int node, int n, int xsize, int ysize){
+    bool result = true;
+    int x, y;
+    node_to_coordinates(node, x, y, xsize, ysize);
+    if( ((x-n)<0) || ((y-n)<0) ){
+        result = false;
+    }
+    return result;
+}
+void recruitment();
+void inactivation();
--- DestructionLib/destructionlib.h
+++ DestructionLib/destructionlib.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream> //getMat
 #include <cmath> //is_integer
+#include <vector> // neighbours
 #include "../constants.hpp"
 using namespace constants;
 
@@ -11,6 +12,14 @@ int rightBorder(int * mat, int size);
 int superiorBorder(int * mat, int xsize, int ysize);
 int inferiorBorder(int * mat, int xsize, int ysize);
 int * effectorCellPlacement(int x0, int y0, int xf, int yf, int xsize, int ysize, int *T);
-void get_coordinates(int node, int xsize, int ysize, int &xcoor, int &ycoor);
+void node_to_coordinates(int node, int &x, int &y, int xsize, int ysize);
 int * get_squeare(int xleft, int xright, int ysup, int yinf, int xsize, int ysize);
 void match_matrices(int * T, int * mat, int xsize, int ysize);
+
+void tumor_lysis(int * T, int * E, int *D, int xsize, int ysize);
+
+void cell_lysis(int * T, int *E, int node, int xsize , int ysize);
+void recruitment();
+void inactivation();
+std::vector<int> n_neighbours(int n, int * mat, int node, int xsize, int ysize);
+bool are_neighbours_interior(int node, int n, int xsize, int ysize);
